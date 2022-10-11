ee373aebe403adb79391cf950824611f02f46e15
LucasMendesM <jpmarquesmendes@gmail.com>
2022-10-11 16:25:11 -0300

    Commit

---
+++ Prática/src/Anatomia.java
@@ -0,0 +1,8 @@
+package Prática.src;
+
+public class Anatomia {
+   // Seu código aqui 
+   public static void main (String [] args) {
+    System.out.print ("Pedindo uma saído ao sistema");
+   }
+}
